6fb2c681a6410b883fad0e14600771d1b031df96
Kyle Harrington <czi@kyleharrington.com>
2024-07-15 19:49:42 -0400

    Update to handle multiple runs and override more of basetileddataset

--- src/morphospaces/datasets/copick.py
+++ src/morphospaces/datasets/copick.py
@@ -6,6 +6,12 @@ import zarr
 from morphospaces.datasets._base import BaseTiledDataset
 from copick.impl.filesystem import CopickRootFSSpec
 
+from morphospaces.datasets.utils import (
+    FilterSliceBuilder,
+    PatchManager,
+    SliceBuilder,
+)
+
 
 class LazyTiledCopickDataset(BaseTiledDataset):
     def __init__(
@@ -32,21 +38,52 @@ class LazyTiledCopickDataset(BaseTiledDataset):
         self.tomo_type = tomo_type
         self.segmentation_name = segmentation_name
         self.voxel_spacing = voxel_spacing
+        self.transform = transform
+        self.patch_shape = patch_shape
+        self.stride_shape = stride_shape
+        self.patch_filter_ignore_index = patch_filter_ignore_index
+        self.patch_threshold = patch_threshold
+        self.patch_slack_acceptance = patch_slack_acceptance
+        self.store_unique_label_values = store_unique_label_values
 
-        super().__init__(
-            patch_filter_key="label",
-            dataset_keys=run_names,
-            file_path=copick_config_path,
-            transform=transform,
-            patch_shape=patch_shape,
-            stride_shape=stride_shape,
-            patch_filter_ignore_index=patch_filter_ignore_index,
-            patch_threshold=patch_threshold,
-            patch_slack_acceptance=patch_slack_acceptance,
-            store_unique_label_values=store_unique_label_values,
-        )
-    
-    def get_array(self, file_path, run_name):
+        self.data = self._load_data()
+        self.patches = self._create_patches()
+
+        if store_unique_label_values:
+            self.unique_label_values = self._get_unique_labels()
+
+    def _load_data(self):
+        data = {}
+        for run_name in self.run_names:
+            data[run_name] = {
+                'raw': self.get_array(run_name, 'raw'),
+                'label': self.get_array(run_name, 'label')
+            }
+        return data
+
+    def _create_patches(self):
+        patches = {}
+        for run_name, run_data in self.data.items():
+            patches[run_name] = PatchManager(
+                data=run_data,
+                patch_shape=self.patch_shape,
+                stride_shape=self.stride_shape,
+                patch_filter_ignore_index=self.patch_filter_ignore_index,
+                patch_filter_key='label',
+                patch_threshold=self.patch_threshold,
+                patch_slack_acceptance=self.patch_slack_acceptance,
+            )
+        return patches
+
+    def _get_unique_labels(self):
+        unique_labels = set()
+        for run_name, run_patches in self.patches.items():
+            for slice_indices in run_patches.slices:
+                label_patch = self.data[run_name]['label'][slice_indices]
+                unique_labels.update(np.unique(label_patch))
+        return list(unique_labels)
+
+    def get_array(self, run_name, key):
         return LazyCopickFile(
             self.copick_config_path,
             run_name,
@@ -54,11 +91,33 @@ class LazyTiledCopickDataset(BaseTiledDataset):
             self.user_id,
             self.tomo_type,
             self.segmentation_name,
-            self.voxel_spacing
+            self.voxel_spacing,
+            key
         )
 
+    def __getitem__(self, idx):
+        run_name = np.random.choice(self.run_names)
+        run_patches = self.patches[run_name]
+        
+        if idx >= len(run_patches):
+            raise StopIteration
+
+        slice_indices = run_patches.slices[idx]
+        data_patch = {
+            key: self.data[run_name][key][slice_indices]
+            for key in ['raw', 'label']
+        }
+
+        if self.transform is not None:
+            data_patch = self.transform(data_patch)
+
+        return data_patch
+
+    def __len__(self):
+        return sum(len(patches) for patches in self.patches.values())
+
 class LazyCopickFile:
-    def __init__(self, copick_config_path, run_name, session_id, user_id, tomo_type, segmentation_name, voxel_spacing):
+    def __init__(self, copick_config_path, run_name, session_id, user_id, tomo_type, segmentation_name, voxel_spacing, key):
         self.copick_config_path = copick_config_path
         self.run_name = run_name
         self.session_id = session_id
@@ -66,56 +125,42 @@ class LazyCopickFile:
         self.tomo_type = tomo_type
         self.segmentation_name = segmentation_name
         self.voxel_spacing = voxel_spacing
-        self.raw_array = None
-        self.label_array = None
-        self._load_arrays()
+        self.key = key
+        self.array = self._load_array()
 
-    def _load_arrays(self):
+    def _load_array(self):
         root = CopickRootFSSpec.from_file(self.copick_config_path)
         run = root.get_run(self.run_name)
         voxel_spacing_obj = run.get_voxel_spacing(self.voxel_spacing)
         
-        # Load raw data
-        tomogram = voxel_spacing_obj.get_tomogram(self.tomo_type)
-        self.raw_array = zarr.open(tomogram.zarr(), mode='r')['0']
-        
-        # Load label data
-        segmentation = run.get_segmentations(
-            user_id=self.user_id,
-            session_id=self.session_id,
-            is_multilabel=True,
-            name=self.segmentation_name,
-            voxel_size=self.voxel_spacing
-        )
-        if segmentation:
-            self.label_array = zarr.open(segmentation[0].zarr(), mode='r')['data']
+        if self.key == 'raw':
+            tomogram = voxel_spacing_obj.get_tomogram(self.tomo_type)
+            return zarr.open(tomogram.path, mode='r')['data']
+        elif self.key == 'label':
+            segmentation = run.get_segmentations(
+                user_id=self.user_id,
+                session_id=self.session_id,
+                is_multilabel=True,
+                name=self.segmentation_name,
+                voxel_size=self.voxel_spacing
+            )
+            if segmentation:
+                return zarr.open(segmentation[0].path, mode='r')['data']
+            else:
+                raise ValueError(f"Segmentation not found for run {self.run_name}")
         else:
-            raise ValueError(f"Segmentation not found for run {self.run_name}")
-        
-        # Set shape and ndim based on raw data
-        self.shape = self.raw_array.shape
-        self.ndim = self.raw_array.ndim
+            raise ValueError(f"Invalid key: {self.key}. Use 'raw' or 'label'.")
 
     def __getitem__(self, arg):
-        if isinstance(arg, str):
-            if arg == 'raw':
-                return self.raw_array
-            elif arg == 'label':
-                return self.label_array
-            else:
-                raise KeyError(f"Invalid key: {arg}. Use 'raw' or 'label'.")
-
-        if arg == Ellipsis:
-            return self
-
-        # For slice indexing, return a dict with both raw and label data
-        return {
-            'raw': self.raw_array[arg],
-            'label': self.label_array[arg]
-        }
+        return self.array[arg]
 
     def ravel(self):
-        return {
-            'raw': self.raw_array.ravel(),
-            'label': self.label_array.ravel()
-        }
+        return self.array.ravel()
+
+    @property
+    def shape(self):
+        return self.array.shape
+
+    @property
+    def ndim(self):
+        return self.array.ndim
